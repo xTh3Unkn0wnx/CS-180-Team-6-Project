8055f064fc5459f7db195a747a9545017facd3da
xTh3Unkn0wnx <aoceg003@ucr.edu>
2024-01-18 15:39:49 -0800

    Added more examples to show communication between backend and frontend

--- main-src/src/client/App.tsx
+++ main-src/src/client/App.tsx
@@ -1,13 +1,33 @@
 import "./App.css";
+import React from "react";
 import { Header } from "./components/Header";
 import { Main } from "./components/Main";
 import { Footer } from "./components/Footer";
 import { useState } from "react";
+import axios from "axios";
 
 import reactLogo from "./assets/react.svg";
+import { stringify } from "querystring";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [message, setMessage] = useState("");
+  const [data, setData] = useState("");
+  axios.get('/api/hello')
+  .then((response) => setMessage(response.data));
+  const handleButton = async () => {
+    if (data != ""){
+      setData("");
+    }
+    else {
+      try {
+        const response = await axios.get('/api/data');
+        setData(response.data)
+      } catch (err) {
+        console.error(`Error fetxhing data:`, err);
+      }
+    }
+  };
 
   return (
     <div className="App">
@@ -15,6 +35,19 @@ function App() {
       <Header></Header>
       <Main></Main>
       <Footer></Footer>
+      <div>
+        <p>
+        {message}
+        </p>
+      </div>
+      <button onClick={handleButton}>
+        Click me
+      </button>
+      <div>
+        {Object.entries(data).map(([key, value]) => (
+          <li key={key}><strong>{key}: </strong>{value}</li>
+        ))}
+      </div>
     </div>
   );
 }
--- main-src/src/server/main.ts
+++ main-src/src/server/main.ts
@@ -1,11 +1,25 @@
+import axios from "axios";
 import express from "express";
 import ViteExpress from "vite-express";
 
 const app = express();
 
-app.get("/hello", (_, res) => {
-  res.send("Hello Vite + React + TypeScript!");
+app.use(express.json());
+
+app.get("/api/hello", (_, res) => {
+  res.send("This is my first message!");
 });
+
+app.get('/api/data',async (req, res) =>  {
+  try {
+    const response = await axios.get('https://jsonplaceholder.typicode.com/posts/1/');
+    const data = response.data;
+    res.json(data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({error: 'Internal Server Error'});
+  }
+})
 
 ViteExpress.listen(app, 3000, () =>
   console.log("Server is listening on port 3000..."),
