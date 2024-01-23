3f31e97c8a66ed93365e006de7e2a9af5709aef9
xTh3Unkn0wnx <aoceg003@ucr.edu>
2024-01-22 23:21:50 -0800

    Modified Some lines in multiple files

--- main-src/src/server/main.ts
+++ main-src/src/server/main.ts
@@ -14,7 +14,6 @@ app.use(cors());
 app.use(express.json());
 
 const URI = process.env.MONGODB_URL;
-console.log(URI);
 if(URI) {
   mongoose.connect(URI)
     const connection = mongoose.connection;
