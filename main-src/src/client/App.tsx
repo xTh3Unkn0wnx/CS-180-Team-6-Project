ab5050c82f73308c54fbd98175b877903bbd0f94
ochen <oceanchen999@gmail.com>
2024-02-01 23:53:56 -0800

    Current the UI is made, needs to be touched up and still need to switch screens on login press

--- main-src/src/client/App.tsx
+++ main-src/src/client/App.tsx
@@ -1,21 +1,28 @@
-import "./App.css";
+  import "./App.css";
 import React from "react";
 import { Header } from "./components/Header";
 import { Main } from "./components/Main";
 import { Footer } from "./components/Footer";
 import { useState } from "react";
 import axios from "axios";
+import ICON from "./assets/icon.png"
 
 import reactLogo from "./assets/react.svg";
 import { stringify } from "querystring";
+import { addSyntheticLeadingComment } from "typescript";
 
 function App() {
   const [count, setCount] = useState(0);
   const [message, setMessage] = useState("");
   const [data, setData] = useState("");
+  const [username, setUsername] = useState("ochen011");
+  const [password, setPassword] = useState("Testpassword123");
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   axios.get('/api/hello')
   .then((response) => setMessage(response.data));
-  const handleButton = async () => {
+
+  const loginButton = async () => {
     if (data != ""){
       setData("");
     }
@@ -29,25 +36,39 @@ function App() {
     }
   };
 
+    const handleLogin = async () => {
+
+    }
+
   return (
     <div className="App">
-      <h1>Hello World!</h1>
-      <Header></Header>
-      <Main></Main>
-      <Footer></Footer>
+      <h1> Live Active </h1>  
+        <img src="src/client/assets/liveActive.jpg" width="200" height="200"/>
+        <div>
+          <h2> Login </h2>
+            <label> 
+              UserName: 
+              <input
+                type="text"
+                value={username}
+              />
+            </label>
+          <br /> 
+          <br /> 
+            <label>
+              Password: 
+              <input
+                type="text"
+                value={password}
+              />
+            </label>
+        </div>
+      <br /> 
       <div>
-        <p>
-        {message}
-        </p>
       </div>
-      <button onClick={handleButton}>
-        Click me
+      <button onClick={loginButton}>
+        Login
       </button>
-      <div>
-        {Object.entries(data).map(([key, value]) => (
-          <li key={key}><strong>{key}: </strong>{value}</li>
-        ))}
-      </div>
     </div>
   );
 }
