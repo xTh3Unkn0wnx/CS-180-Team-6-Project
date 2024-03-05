b77b511af1e71d4ff86bcb2cdbc72147635780e3
Ocean Chen <ochen011@ucr.edu>
2024-03-04 21:16:54 -0800

    Almost complete with home screen UI

--- main-src/src/client/homeScreen.tsx
+++ main-src/src/client/homeScreen.tsx
@@ -20,49 +20,62 @@ const Home = () => {
                                 <a href=""> Schedule Workout </a>
                                 <a href=""> View Calendar </a>
                                 <a href=""> Create Meal Plan </a>
+                                <a href=""> Search For Exercise </a>
+                                <a href=""> Search a Meal </a>
+                                <a href=""> View Meal </a>
+                                <a href=""> Add Meal </a>
                             </div>
                         </li>
-                        <li><a href="" className="links">  </a></li>
                         <li><a href="" className="links"> About Us </a></li>
                         <li><a href="" className="links"> Contact Us </a></li>
                     </ul>
                 </div>
             </div>
 
-            <div className="featureContainer"> 
-                <a href="/login" className="clickableContainer"> 
-                    <div className="calendarBox"> 
-                        <div className="textBottomWorkout"> Schedule Workout</div>
-                    </div>
-                </a>
-
-                <a href="" className="clickableContainer"> 
-                    <div className="mealBox">
-                        <div className="textBottomMeal"> Create Meal Plan </div>
-                    </div>
-                </a>
-
-                <a href="" className="clickableContainer"> 
-                    <div className="searchExerciseBox">
-                        <div className="textBottomSearchExercise"> Search Exercise </div>
-                    </div>
-                </a>
-                
-                <div className="rowSpacing">
-                    <a href="" className="clickableContainer"> 
-                        <div className="searchMealBox">
-                            <div className="textBottomSearchMeal"> Search Meals </div>
+            <div className="homeScreenContainer">
+                <div className="featureContainer"> 
+                    <a href="/login" className="clickableContainer"> 
+                        <div className="calendarBox"> 
+                            <div className="textBottomWorkout"> Schedule Workout</div>
                         </div>
+                    </a>
+
+                    <a href="" className="clickableContainer"> 
+                        <div className="mealBox">
+                            <div className="textBottomCreateMeal"> Create Meal Plan </div>
+                        </div>
+                    </a>
+
+                    <a href="" className="clickableContainer"> 
+                        <div className="searchExerciseBox">
+                            <div className="textBottomSearchExercise"> Search Exercise </div>
+                        </div>
+                    </a>
+
+                        <a href="" className="clickableContainer"> 
+                            <div className="searchMealBox">
+                                <div className="textBottomSearchMeal"> Search Meal </div>
+                            </div>
+                        </a>
+
+                    <a href="" className="clickableContainer"> 
+                            <div className="viewMealBox">
+                                <div className="textBottomViewMeal"> View Meal </div>
+                            </div>
+                    </a>
+
+                    <a href="" className="clickableContainer"> 
+                            <div className="addMealBox">
+                                <div className="textBottomAddMeal"> Add Meal </div>
+                            </div>
                     </a>
                 </div>
 
-                <a href="" className="clickableContainer"> 
-                        <div className="viewMealBox">
-                            <div className="textBottomViewMeal"> Search Meals </div>
-                        </div>
-                </a>
+                <div className="welcomeContainer"> 
+                    <div className="welcomeContainerText"> Welcome Back! Ready to have a good workout? Select a something to get started!</div>
+                </div>
+                <div className="sidePicture"></div>
             </div>
-
         </body>
     ); 
 }
