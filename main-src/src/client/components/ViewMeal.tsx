2da6f054f4c611139cbdb91b012e78f0df86726d
Andrew Ocegueda <andrewocegueda2018@gmail.com>
2024-03-14 15:10:29 -0700

    Add style to ViewMeal.tsx

--- main-src/src/client/components/ViewMeal.tsx
+++ main-src/src/client/components/ViewMeal.tsx
@@ -1,30 +1,68 @@
-
 import { MealDocument } from "../interface/MealDocument";
 import defaultFoodIcon from "../assets/defaultFoodIcon.jpg";
 import "./ViewMeal.css";
 
-export const Viewmeal = ({meals, deleteMeal} : {meals : MealDocument[], deleteMeal: (mealId: string) => void}) => {
+export const Viewmeal = ({
+  meals,
+  deleteMeal,
+}: {
+  meals: MealDocument[];
+  deleteMeal: (mealId: string) => void;
+}) => {
   // const [meals, setMeals] = useState<MealDocument[]>([]);
 
   return (
-        <div className="meal-container">
-          {meals.map((meal) => (
-            <div className="meal-card" key={meal._id}>
-              <h3>{meal.mealName}</h3>
-              <img
-                src={
-                  meal.urlImage && meal.urlImage.trim() !== ""
-                    ? meal.urlImage
-                    : defaultFoodIcon
-                }
-                alt="Meal"
-              />
-              <p>{meal.type}</p>
-              <p>{meal.calories}</p>
-              <button className="delete-button" onClick={() => deleteMeal(meal._id)}>delete entry</button>
-            </div>
-          ))}
+    <div className="meal-container">
+      {meals.map((meal) => (
+        <div className="meal-card" key={meal._id}>
+          <h3>{meal.mealName}</h3>
+          <img
+            src={
+              meal.urlImage && meal.urlImage.trim() !== ""
+                ? meal.urlImage
+                : defaultFoodIcon
+            }
+            alt="Meal"
+          />
+          <p>
+            <span
+              style={{
+                fontWeight: "bold",
+              }}
+            >
+              Meal Type:
+            </span>{" "}
+            {meal.type}
+          </p>
+          <p>
+            <span
+              style={{
+                fontWeight: "bold",
+              }}
+            >
+              Calories:
+            </span>{" "}
+            {meal.calories}
+          </p>
+          <p>
+            <span
+              style={{
+                fontWeight: "bold",
+              }}
+            >
+              Description:
+            </span>{" "}
+            {meal.description}
+          </p>
+          <button
+            className="delete-button"
+            onClick={() => deleteMeal(meal._id)}
+          >
+            delete entry
+          </button>
         </div>
+      ))}
+    </div>
   );
 };
 
